97ce834b306c853112be260d7c049fdfd33660e7
germanmattias <38841129+germanmattias@users.noreply.github.com>
2022-02-25 19:34:17 -0300

    se agrego logica para guardar datos en localstorage

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,48 @@
-import {Juegos} from './gameClass.js';
+import { Juegos } from "./gameClass.js";
 
-let nuevoJuego = new Juegos(1, 'asd', 'asd', 'asd','asd','asd');
-console.log(nuevoJuego);
+let listaJuegos = [];
+
+window.agregarJuego = function (event) {
+  event.preventDefault();
+  if (
+    validarCodigo(document.getElementById("codigoJuego")) &&
+    validarCampoRequerido(document.getElementById("codigoJuego")) &&
+    validarCampoRequerido(document.getElementById("serieJuego")) &&
+    validarCampoRequerido(document.getElementById("categoriaJuego")) &&
+    validarCampoRequerido(document.getElementById("descripcionJuego")) &&
+    validarCampoRequerido(document.getElementById("imagenJuego"))
+  ) {
+    console.log("esta bien");
+    //crear un nuevo juego
+    let nuevoJuego = new Juegos(
+      document.getElementById("codigoJuego").value,
+      document.getElementById("codigoJuego").value,
+      document.getElementById("serieJuego").value,
+      document.getElementById("categoriaJuego").value,
+      document.getElementById("descripcionJuego").value,
+      document.getElementById("imagenJuego").value
+    );
+    //guardar el juego en el array
+    console.log(nuevoJuego);
+    listaJuegos.push(nuevoJuego);
+    //guardar los datos en LS
+    localStorage.setItem("listajuegoskey", JSON.stringify(listaJuegos));
+    //mostrar alert de datos guardados
+    Swal.fire(
+      'Nuevo Juego',
+      'El juego se agrego correctamente',
+      'success'
+    )
+    //limpiar el formulario
+    limpiarFormulario();
+
+  } else {
+    console.log("esta mal");
+  }
+}
+
+function limpiarFormulario() {
+  let formulario = document.getElementById('formJuego').reset();
+  //limpiar la class de los input
+
+}
